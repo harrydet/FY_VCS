d327149847e3f60035f43f5a34b834cc14fdacea
Harry <harry.det@hotmail.com>
2014-12-02 17:29:02 +0000

    Test Commit

--- SensorGUI/SensorGUI/MainUI.xaml.cs
+++ SensorGUI/SensorGUI/MainUI.xaml.cs
@@ -19,6 +19,7 @@ namespace SensorGUI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         private int isConnected;
