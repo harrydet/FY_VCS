90b615d744d9fdbf0bbe8bc56451de6ccdb15164
Harry <harry.det@hotmail.com>
2015-01-16 04:15:18 +0000

    RFID Reader Recognised. Changed status indicators

--- SensorGUI/SensorGUI/MainUI.xaml.cs
+++ SensorGUI/SensorGUI/MainUI.xaml.cs
@@ -24,10 +24,17 @@ namespace SensorGUI
     /// 
     public partial class MainWindow : Window
     {
-        private int isConnected;
+        private int isConnectedReader;
+        private int isConnectedTag;
         private RFID rfid; //Declare an RFID object
         private ErrorEventBox errorBox;
-        enum States
+        enum Reader_States
+        {
+            DISCONNECTED = 0,
+            CONNECTING = 1,
+            CONNECTED = 2
+        };
+        enum Tag_States
         {
             DISCONNECTED = 0,
             CONNECTING = 1,
@@ -44,7 +51,7 @@ namespace SensorGUI
         public int Initialization()
         {
             errorBox = new ErrorEventBox();
-            isConnected = (int)States.CONNECTED;
+            isConnectedReader = (int)Reader_States.DISCONNECTED;
             return 0;
         }
 
@@ -55,48 +62,51 @@ namespace SensorGUI
 
         public void updateStatus()
         {
-            SolidColorBrush onBrush = new SolidColorBrush();
-            SolidColorBrush offBrush = new SolidColorBrush();
+            SolidColorBrush readerBrush = new SolidColorBrush();
+            SolidColorBrush tagBrush = new SolidColorBrush();
 
-            if (isConnected == (int)States.DISCONNECTED)
+            switch (isConnectedReader)
             {
-                offBrush.Color = Color.FromArgb(255, 255, 0, 0);
-                StatusOFF.Fill = offBrush;
-                onBrush.Color = Color.FromArgb(255, 255, 255, 255);
-                StatusON.Fill = onBrush;
+                case (int)Tag_States.DISCONNECTED:
+                    readerBrush.Color = Color.FromArgb(255, 255, 0, 0);
+                    statusReader.Fill = readerBrush;
+                    break;
+                case (int)Tag_States.CONNECTING:
+                    readerBrush.Color = Color.FromArgb(255, 255, 255, 0);
+                    statusReader.Fill = readerBrush;
+                    break;
+                case (int)Tag_States.CONNECTED:
+                    readerBrush.Color = Color.FromArgb(255, 0, 255, 0);
+                    statusReader.Fill = readerBrush;
+                    break;
+                default:
+                    break;
             }
-            else if (isConnected == (int)States.CONNECTING)
+
+            switch (isConnectedTag)
             {
-                offBrush.Color = Color.FromArgb(255, 255, 255, 0);
-                StatusOFF.Fill = offBrush;
-                onBrush.Color = Color.FromArgb(255, 255, 255, 255);
-                StatusON.Fill = onBrush;
+                case (int)Tag_States.DISCONNECTED:
+                    tagBrush.Color = Color.FromArgb(255, 255, 0, 0);
+                    statusTag.Fill = tagBrush;
+                    break;
+                case (int)Tag_States.CONNECTING:
+                    tagBrush.Color = Color.FromArgb(255, 255, 255, 0);
+                    statusTag.Fill = tagBrush;
+                    break;
+                case (int)Tag_States.CONNECTED:
+                    tagBrush.Color = Color.FromArgb(255, 0, 255, 0);
+                    statusTag.Fill = tagBrush;
+                    break;
+                default:
+                    break;
             }
-            else
-            {
-                onBrush.Color = Color.FromArgb(255, 0, 255, 0);
-                StatusON.Fill = onBrush;
-                offBrush.Color = Color.FromArgb(255, 255, 255, 255);
-                StatusOFF.Fill = offBrush;
-            }
-            onBrush = null;
-            offBrush = null;
+            readerBrush = null;
+            tagBrush = null;
         }
 
-        private void SimulateButton_Click(object sender, RoutedEventArgs e)
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
-            if (isConnected == (int)States.DISCONNECTED)
-            {
-                isConnected = (int)States.CONNECTING;
-            }
-            else if (isConnected == (int)States.CONNECTING)
-            {
-                isConnected = (int)States.CONNECTED;
-            }
-            else
-            {
-                isConnected = (int)States.DISCONNECTED;
-            }
+
             updateStatus();
         }
 
@@ -116,23 +126,44 @@ namespace SensorGUI
             rfid.Tag += new TagEventHandler(rfid_Tag);
             rfid.TagLost += new TagEventHandler(rfid_TagLost);
 
+            openCmdLine(rfid, null);
+
         }
 
         void rfid_Attach(object sender, AttachEventArgs e)
         {
             RFID attached = (RFID)sender;
-            Reader_Information.Text += attached.Attached.ToString();
-            Reader_Information.Text += "\nName: " + attached.Name;
-            isConnected = (int)States.CONNECTED;
-            updateStatus();
+            try
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    Reader_Information.Text = "Name: " + attached.Name;
+                    isConnectedReader = (int)Reader_States.CONNECTING;
+                    updateStatus();
+                }));
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
         }
 
         void rfid_Detach(object sender, DetachEventArgs e)
         {
-            RFID detached = (RFID)sender;
-            Reader_Information.Text = String.Empty;
-            isConnected = (int)States.DISCONNECTED;
-            updateStatus();
+            try
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    RFID detached = (RFID)sender;
+                    Reader_Information.Text = String.Empty;
+                    isConnectedReader = (int)Reader_States.DISCONNECTED;
+                    updateStatus();
+                }));
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
         }
 
         void rfid_Error(object sender, ErrorEventArgs e)
@@ -172,6 +203,89 @@ namespace SensorGUI
             rfid.close();
         }
 
-        
+        #region Command line open functions
+        private void openCmdLine(Phidget p, String pass)
+        {
+            int serial = -1;
+            String logFile = null;
+            int port = 5001;
+            String host = null;
+            bool remote = false, remoteIP = false;
+            string[] args = Environment.GetCommandLineArgs();
+            String appName = args[0];
+
+            try
+            { //Parse the flags
+                for (int i = 1; i < args.Length; i++)
+                {
+                    if (args[i].StartsWith("-"))
+                        switch (args[i].Remove(0, 1).ToLower())
+                        {
+                            case "l":
+                                logFile = (args[++i]);
+                                break;
+                            case "n":
+                                serial = int.Parse(args[++i]);
+                                break;
+                            case "r":
+                                remote = true;
+                                break;
+                            case "s":
+                                remote = true;
+                                host = args[++i];
+                                break;
+                            case "p":
+                                pass = args[++i];
+                                break;
+                            case "i":
+                                remoteIP = true;
+                                host = args[++i];
+                                if (host.Contains(":"))
+                                {
+                                    port = int.Parse(host.Split(':')[1]);
+                                    host = host.Split(':')[0];
+                                }
+                                break;
+                            default:
+                                goto usage;
+                        }
+                    else
+                        goto usage;
+                }
+                if (logFile != null)
+                    Phidget.enableLogging(Phidget.LogLevel.PHIDGET_LOG_INFO, logFile);
+                if (remoteIP)
+                    p.open(serial, host, port, pass);
+                else if (remote)
+                    p.open(serial, host, pass);
+                else
+                    p.open(serial);
+                return; //success
+            }
+            catch {
+                System.Windows.MessageBox.Show("Oops");
+            }
+        usage:
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Invalid Command line arguments." + Environment.NewLine);
+            sb.AppendLine("Usage: " + appName + " [Flags...]");
+            sb.AppendLine("Flags:\t-n   serialNumber\tSerial Number, omit for any serial");
+            sb.AppendLine("\t-l   logFile\tEnable phidget21 logging to logFile.");
+            sb.AppendLine("\t-r\t\tOpen remotely");
+            sb.AppendLine("\t-s   serverID\tServer ID, omit for any server");
+            sb.AppendLine("\t-i   ipAddress:port\tIp Address and Port. Port is optional, defaults to 5001");
+            sb.AppendLine("\t-p   password\tPassword, omit for no password" + Environment.NewLine);
+            sb.AppendLine("Examples: ");
+            sb.AppendLine(appName + " -n 50098");
+            sb.AppendLine(appName + " -r");
+            sb.AppendLine(appName + " -s myphidgetserver");
+            sb.AppendLine(appName + " -n 45670 -i 127.0.0.1:5001 -p paswrd");
+            System.Windows.MessageBox.Show(sb.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            System.Windows.Application.Current.Shutdown();
+        }
+        #endregion
+
+
     }
 }
